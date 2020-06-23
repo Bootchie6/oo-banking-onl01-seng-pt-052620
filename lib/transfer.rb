c0b3bef97eabc352510956a7df66c53edc85a25e
Jonathan Jopio <jonathanjopio@gmail.com>
2020-06-23 03:15:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,7 @@
 class Transfer
-  attr_reader
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  
   def initialize(sender, receiver, amount)
     @status = "pending"
     @sender = sender
